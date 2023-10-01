a767e6a30520eebcc30fc5ff69722988865aa6b2
Anders Reiche <andersreiche@gmail.com>
2023-10-01 23:53:27 +0200

    twoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const stepSize = Math.floor(Math.sqrt(breaks.length));
+    for (let i = 0; i < breaks.length; i += stepSize) {
+        if (breaks[i]) {
+            i -= stepSize;
+            for (i; i < breaks.length; i++) {
+                if (breaks[i]) {
+                    return i;
+                }
+            }
+        }
+    }
+    return -1;
 }
