c3783e15e045a1c0861e1ab3275be86667548f8a
Anders Reiche <andersreiche@gmail.com>
2023-10-01 23:45:05 +0200

    BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        let midpoint = Math.floor(low + (high - low) / 2);
+        let val = haystack[midpoint];
+        if (val === needle) {
+            return true;
+        } else if (val < needle) {
+            low = midpoint + 1;
+        } else {
+            high = midpoint;
+        }
+    } while (low < high);
+    return false;
 }
