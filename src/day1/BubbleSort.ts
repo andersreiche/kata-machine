4df5c68f31e21bbd62b8246578e378544a4070fa
Anders Reiche <andersreiche@gmail.com>
2023-10-02 20:09:42 +0200

    bubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
+    let len = arr.length;
 
+    while (len > 1) {
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // swap
+                const tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+            }
+        }
+        len--;
+    }
 }
