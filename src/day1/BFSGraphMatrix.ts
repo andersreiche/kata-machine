fc38f89d432aa6daea36b929ad051f139a744c42
Anders Reiche <andersreiche@gmail.com>
2023-10-04 20:38:01 +0200

    breadth first search graph matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,41 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+    const q: number[] = [source];
+
+    // find needle
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        const adjacencies = graph[curr];
+        for (let i = 0; i < adjacencies.length; i++) {
+            if (adjacencies[i] === 0) {
+                continue;
+            }
+            if (seen[i]) {
+                continue;
+            }
+            prev[i] = curr;
+            seen[i] = true;
+            q.push(i);
+        }
+    } while (q.length);
+
+    // build path backwards
+    let curr = needle;
+    const out: number[] = [];
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    if (out.length) {
+        out.push(source);
+        return out.reverse();
+    }
+    return null;
 }
